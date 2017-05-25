8f1857d1cb8c8a8010bb75c4d5a0d50b91c44181
screenflip <screenflip@gmail.com>
2017-05-25 18:32:43 +0900

    fix for

--- 04_for.py
+++ 04_for.py
@@ -28,3 +28,24 @@ else:
     print(n, "は素数です")
 # else は for の中で break しなかった場合に実行される
 # for の中身が1度も実行されなくても実行されるので注意
+#for i in range(10):
+#  print(i)
+#=>0 1 2 3 4 5 6 7 8 9
+
+#for i in range(5,10):
+#  print(i)
+#=>5 6 7 8 9
+
+#for i in range(1,10,2):
+#  print(i)
+#=>1 3 5 7 9
+#増加量を決められる
+
+for i in range(10, 0, -1):
+  print(i)
+  if i<5:
+      break
+#=>10 9 8 7 6 5 4 3 2 1
+#負の増加量も扱える
+
+print(i)
