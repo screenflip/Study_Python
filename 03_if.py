14fafcedf563002393ea0ae391e5d2e08e324ca3
screenflip <screenflip@gmail.com>
2017-05-25 18:03:26 +0900

    add if

---
+++ 03_if.py
@@ -0,0 +1,15 @@
+# coding:shift-jis
+
+n = 11
+
+if n % 15 == 0:
+    print("Fizz Buzz")
+elif n % 3 == 0:
+    print("Fizz")
+elif n % 5 == 0:
+    print("Buzz")
+else:
+    print(n)
+# インデントに注意
+# インデントの前には:を書く
+# else if の代わりに elif が使う
