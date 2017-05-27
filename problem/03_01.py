87dc0a676de182a4e0d442341bb647db146fb8c7
screenflip <screenflip@gmail.com>
2017-05-27 17:32:47 +0900

    fix problem/03_01.py

--- problem/03_01.py
+++ problem/03_01.py
@@ -2,10 +2,10 @@
 
 for n in range(1,101):
     if n%15==0:
-        print("Fizz Bazz")
+        print("Fizz Buzz")
     elif n%3==0:
         print("Fizz")
     elif n%5==0:
-        print("Bazz")
+        print("Buzz")
     else:
         print(n)
