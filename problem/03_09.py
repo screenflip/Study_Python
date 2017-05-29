2169953b84957b711fc5998b12af783f5dde9030
screenflip <screenflip@gmail.com>
2017-05-29 14:24:06 +0900

    add problem/isper.py, problem.03_09.py

---
+++ problem/03_09.py
@@ -0,0 +1,11 @@
+#coding:shift-jis
+
+from isper import is_perfect
+
+def my_filter(rang,func):
+    for i in rang:
+        if func(i):
+            print(i)
+
+
+my_filter(range(1,1000),is_perfect)
