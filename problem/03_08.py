e39e6c60e15b705ede853242fc52548f15eee2a5
Yuki Yamashita <yyama0704@gmail.com>
2017-05-29 17:27:38 +0900

    check 03_*

--- problem/03_08.py
+++ problem/03_08.py
@@ -1,10 +1,12 @@
 #coding:shift-jis
 
 def is_perfect(n):
+#   sum は組み込み関数なので(ry
     sum=0
     for i in range(1,n+1):
         if n%i==0:
             sum+=i
+#   0は完全数じゃないから and n != 0 のほうが良いかも？
     if sum==2*n or n==0:
         return True
     else:
@@ -13,6 +15,7 @@ def is_perfect(n):
 if __name__=="__main__":
     n_max=200
     for n in range(1,n_max+1):
+#       end='\t' で簡単に表を作れるの便利ですね！
         print(n,":",is_perfect(n),end="\t")
         if n%5==0:
             print()
